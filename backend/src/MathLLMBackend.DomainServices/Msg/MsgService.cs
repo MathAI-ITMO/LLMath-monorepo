9aec000ff4af85ba447d23e80a9e3b1a164218c6
Ivan Evtushenko <eid20021@gmail.com>
2025-03-05 01:31:48 +0300

    multiple fixes

--- backend/src/MathLLMBackend.DomainServices/Msg/MsgService.cs
+++ backend/src/MathLLMBackend.DomainServices/Msg/MsgService.cs
@@ -1,6 +1,11 @@
+using System.ClientModel;
+using System.Diagnostics;
 using MathLLMBackend.Domain.Entities;
+using MathLLMBackend.Domain.Enums;
 using MathLLMBackend.Domain.Exceptions;
 using MathLLMBackend.Repository;
+using OpenAI;
+using OpenAI.Chat;
 
 namespace MathLLMBackend.DomainServices.MessageService;
 
@@ -19,10 +24,34 @@ public class MessageService : IMessageService
     {
         if (!await _chatRepository.IsChatExistsForUser(userId, message.ChatId, ct))
             throw new AuthorizationException($"User {userId} does not have access to chat {message.ChatId}");
-
+        
         var newMessage = await _messageRepository.Create(message, ct)
             ?? throw new InvalidOperationException("Unexpected error in Creating message"); 
-        return newMessage;
+        
+        var messages = await _messageRepository.GetAllMessageFromChat(message.ChatId, ct)
+            ?? throw new InvalidOperationException("Unexpected error in getting messages"); 
+
+        var client = new ChatClient (
+            model: Environment.GetEnvironmentVariable("OPENAI_MODEL"),
+            credential: new ApiKeyCredential(Environment.GetEnvironmentVariable("OPENAI_API_KEY")),
+            options: new OpenAIClientOptions() { Endpoint =  new Uri(Environment.GetEnvironmentVariable("OPENAI_API_ENDPOINT"))} );
+
+        var openaiMessages = messages.Select<Message, ChatMessage>(m =>
+            m.MessageType switch
+            {
+                MessageType.User => new UserChatMessage(m.Text),
+                MessageType.Assistant => new AssistantChatMessage(m.Text),
+                MessageType.System => new SystemChatMessage(m.Text),
+                _ => throw new NotImplementedException()
+            }
+        );
+
+        ChatCompletion completion = client.CompleteChat(openaiMessages);
+
+        var resultMessage = new Message(message.ChatId, completion.Content[0].Text, MessageType.Assistant);
+
+        await _messageRepository.Create(resultMessage, ct);
+        return resultMessage;
     }
 
     public async Task<List<Message>> GetAllMessageFromChat(long userId, long chatId, CancellationToken ct)
