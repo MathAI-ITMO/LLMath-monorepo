dbbc7f6a557d406eb8bbe4b8b4c6b2495c5ef208
Ivan Evtushenko <eid20021@gmail.com>
2025-03-24 20:08:58 +0300

    delete old migrations

--- backend/migrations/00004_add_chat_messages_type_enum.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-create type message_type as enum('system', 'user', 'assistant');
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-drop type if exists message_type;
--- +goose StatementEnd
