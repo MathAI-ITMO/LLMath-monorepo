789ee1cb5a4f0154ff59a1b0e8a9978eb482efa2
Ivan Evtushenko <eid20021@gmail.com>
2026-03-21 17:56:12 +0300

    docker comopose with nginx

---
+++ config.docker.js
@@ -0,0 +1,14 @@
+window.APP_CONFIG = {
+  baseUrl: '/',
+
+  services: {
+    videoServiceUrl: '/video',
+  },
+
+  features: {},
+
+  meta: {
+    version: '1.0.0',
+    environment: 'production',
+  }
+};
